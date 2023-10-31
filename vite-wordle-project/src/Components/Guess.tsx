7f33688ae62ee190aa73d0560610799130ec3dcc
peipeix2 <peipei.hsueh@outlook.com>
2023-10-31 10:07:23 +0800

    Add game UI layout

---
+++ vite-wordle-project/src/Components/Guess.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  word: string,
+  guess: string,
+  isGuessed: boolean
+}
+
+function Guess ({word, guess, isGuessed}: Props) {
+   
+
+  return(
+    <div className="flex gap-2 mb-2">
+      {Array(5).fill(0).map((_, index) => {
+      return (
+        <div className='flex items-center justify-center border w-12 h-12 text-white font-bold text-3xl'>{guess.toUpperCase()[index]}</div>
+      )
+    })}
+    </div>
+  )
+}
+
+export default Guess
